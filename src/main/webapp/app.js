74d254b552211f21f8f82a9dfc970126a9e52173
Borislav Draganov <bobodraganov@gmail.com>
2016-05-17 21:48:03 +0300

    Improved UI a little

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -6,8 +6,12 @@ app.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 		when('/', {
-			templateUrl: '/views/list.html',
-			controller: 'ListCtrl'
+			templateUrl: '/views/main.html',
+			controller: 'MainCtrl'
+		}).
+		when('/dvds', {
+			templateUrl: '/views/dvds.html',
+			controller: 'DvdCtrl'
 		}).
 		when('/:model', {
 			templateUrl: '/views/list.html',
@@ -17,6 +21,16 @@ app.config(['$routeProvider',
 			redirectTo: '/'
 		});
 	}]);
+	
+app.controller('MainCtrl', function($scope) {
+	$scope.list = [
+		'dvd',
+		'movies',
+		'languages',
+		'audios',
+		'genres'
+	];
+});
   
 app.controller('ListCtrl', function($scope, $http, $routeParams) {
 	$scope.list = [];
@@ -37,7 +51,7 @@ app.controller('ListCtrl', function($scope, $http, $routeParams) {
 		if ($scope.list.length > 0) {
 			$scope.properties = Object.keys(res.data[0]);
 			var index = $scope.properties.indexOf("id");
-			$scope.properties.splice(0, 1);
+			$scope.properties.splice(index, 1);
 			console.log('properties', $scope.properties);
 		}
 	})
@@ -61,19 +75,20 @@ app.controller('ListCtrl', function($scope, $http, $routeParams) {
 	$scope.saveItem = function($index) {
 		var item = $scope.list[$index];
 
+		function saveCallback() {
+			console.log('Item saved');
+			$scope.editModes[$index] = false;
+		}
+		
 		// Update
 		if (item.id) {
 			$http.put(ROOT_URL + $routeParams.model + '/' + item.id, item)
-			.then((data) => {
-				console.log('Item saved');
-			});
+			.then(saveCallback);
 		}
 		// Create new
 		else {
 			$http.post(ROOT_URL + $routeParams.model, item)
-			.then((data) => {
-			console.log('Item saved');
-			});
+			.then(saveCallback);
 		}
 	};
 	
@@ -83,6 +98,94 @@ app.controller('ListCtrl', function($scope, $http, $routeParams) {
 		$http.delete(ROOT_URL + $routeParams.model + '/' + item.id, item)
 			.then((data) => {
 				console.log('Item deleted');
+				$scope.list.splice($index, 1);
+				$scope.editModes.splice($index, 1);
+			});
+	};
+});
+
+app.controller('DvdCtrl', function($scope, $http, $routeParams) {
+	$scope.list = [];
+	$scope.editModes = [];
+	$scope.properties = [];
+
+	$http.get(ROOT_URL + 'dvds').then((res) => {
+		console.log(res);
+		
+		$scope.list = res.data;
+		
+		for (var item of $scope.list) {
+			$scope.editModes.push(false);
+			
+			item.audioIds = '';
+			for (var audio in item.audios) {
+				item.audioIds = audio +',' + item.audioIds;
+			}
+			item.langIds = '';
+			for (var lang in item.languages) {
+				item.langIds =  lang +',' + item.langIds;
+			}
+			item.movieId = item.movie.id;
+			item.movieTitle = item.movie.title;
+			delete item.movie;
+		}
+		
+		if ($scope.list.length > 0) {
+			$scope.properties = Object.keys(res.data[0]);
+			var index = $scope.properties.indexOf("id");
+			$scope.properties.splice(index, 1);
+			var languages = $scope.properties.indexOf("languages");
+			$scope.properties.splice(languages, 1);
+			var audios = $scope.properties.indexOf("audios");
+			$scope.properties.splice(audios, 1);
+			console.log('properties', $scope.properties);
+		}
+	})
+	
+	$scope.message = 'Main';
+	
+	$scope.changeEditMode = function($index) {
+		console.log('changeEditMode', $index);
+		$scope.editModes[$index] = !$scope.editModes[$index];
+	};
+	
+	$scope.showDetails = function($index) {
+		return $scope.editModes[$index];
+	};
+
+	$scope.addItemToList = function() {
+		$scope.list.push({});
+		$scope.editModes.push(true);
+	};
+	
+	$scope.saveItem = function($index) {
+		var item = $scope.list[$index];
+
+		function saveCallback() {
+			console.log('Item saved');
+			$scope.editModes[$index] = false;
+		}
+		
+		// Update
+		if (item.id) {
+			$http.put(ROOT_URL + 'dvds/' + item.id, item)
+			.then(saveCallback);
+		}
+		// Create new
+		else {
+			$http.post(ROOT_URL + $routeParams.model, item)
+			.then(saveCallback);
+		}
+	};
+	
+	$scope.deleteItem = function($index) {
+		var item = $scope.list[$index];
+		
+		$http.delete(ROOT_URL + 'dvds/' + item.id, item)
+			.then((data) => {
+				console.log('Item deleted');
+				$scope.list.splice($index, 1);
+				$scope.editModes.splice($index, 1);
 			});
 	};
 });
